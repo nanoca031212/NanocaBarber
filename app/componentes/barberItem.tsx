bbad595506cb7dfbbd1d00d847b66e2a1e836af4
nanoca031212 <lucasnatanael542@gmail.com>
2026-02-21 12:47:13 -0300

    criado pagina de pedido funcionando porem nao pronta

--- app/componentes/barberItem.tsx
+++ app/componentes/barberItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 import type { Barbershop } from "@/generated/prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface SectionProps {
   barbershop: Barbershop;
@@ -8,28 +9,25 @@ interface SectionProps {
 
 const BarberItem = ({ barbershop }: SectionProps) => {
   return (
-    <div
-      className="relative min-h-[200px] min-w-[290px] rounded-xl"
-      onClick={() => {
-        window.location.href = `/pedidos/${barbershop.id}`;
-      }}
-    >
-      <div className="absolute top-0 right-0 w-full h-full bg-linear-to-t  from-black to-transparent"></div>
-      <Image
-        src={barbershop.imageUrl}
-        alt={barbershop.name}
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="h-auto w-[365]"
-      />
-      <div className="absolute bottom-0 left-0 w-full h-full flex flex-col justify-end items-start  p-4">
-        <h1 className="text-white font-bold text-base">{barbershop.name}</h1>
-        <h1 className="text-white font-regular text-xs">
-          {barbershop.address}
-        </h1>
+    <Link href={`/pedidos/${barbershop.id}`}>
+      <div className="relative min-h-[200px] min-w-[290px] rounded-xl">
+        <div className="absolute top-0 right-0 w-full h-full bg-linear-to-t  from-black to-transparent"></div>
+        <Image
+          src={barbershop.imageUrl}
+          alt={barbershop.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-[365]"
+        />
+        <div className="absolute bottom-0 left-0 w-full h-full flex flex-col justify-end items-start  p-4">
+          <h1 className="text-white font-bold text-base">{barbershop.name}</h1>
+          <h1 className="text-white font-regular text-xs">
+            {barbershop.address}
+          </h1>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 export default BarberItem;
--- app/pedidos/[id]/page.tsx
+++ app/pedidos/[id]/page.tsx
@@ -1,11 +1,43 @@
-interface PedidoProps {
-  id: string;
-  name: string;
-  imageUrl: string;
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+// Inicializa o Prisma
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+
+// Interface para o que vem da URL
+interface PageProps {
+  params: Promise<{ id: string }>;
 }
 
-const pedido = ({ params }: { params: PedidoProps }) => {
-  return <h1>Pedido {params.name}</h1>;
-};
+export default async function PedidoPage({ params }: PageProps) {
+  // 1. Pega o ID da URL
+  const { id } = await params;
 
-export default pedido;
+  // 2. Busca no banco e GUARDA na variável 'barbershop'
+  const barbershop = await prisma.barbershop.findUnique({
+    where: { id },
+  });
+
+  // 3. Se não encontrar, para a execução e mostra 404
+  if (!barbershop) {
+    return notFound();
+  }
+
+  // 4. Agora a variável 'barbershop' existe e pode ser usada abaixo
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Barbearia: {barbershop.name}</h1>
+      <Image
+        src={barbershop.imageUrl}
+        alt={barbershop.name}
+        width={200}
+        height={200}
+        className="rounded-xl"
+      />
+      <p className="text-gray-400">{barbershop.address}</p>
+    </div>
+  );
+}
